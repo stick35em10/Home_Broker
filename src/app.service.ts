064fa675102378d3c9635795d818acdd78d6824b
nelca <nelca@mail.com>
2025-02-21 01:16:37 +0200

    modified: src/app.service.ts cmd.txt

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'modified:   src/app.service.ts, ! Ao Pai agradou darvos o reino! Busca o Reino e a sua Justica e tudo o resto lhe sera acrescentado!';
   }
 }
